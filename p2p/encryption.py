33710dc33f1fd5bcbce7c211174a72bd24c96792
joshl1-umbc <146989850+joshl1-umbc@users.noreply.github.com>
2023-10-17 19:38:43 +0000

    Update encryption.py (uploaded wrong file the first time. )

--- p2p/encryption.py
+++ p2p/encryption.py
@@ -5,6 +5,37 @@ import base64
 import zlib
 import bz2
 import lzma
+a# ############################################
+# Created by: Josh Li
+# Date      : 2023
+# Class     : CMSC 626
+# Project   : P2P
+# ############################################
 
+from cryptography.fernet import Fernet
+from nodeconnection import node_connection
+
+class node_connection_encryption(node_connection):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def encrypt_data(self, data):
+        try:
+            fernet = Fernet(self.encryption_key)
+            encrypted_data = fernet.encrypt(data)
+            return encrypted_data
+        except Exception as e:
+            self.parent_node.debug_message(f"Encryption error: {e}")
+            return None
+
+    def decrypt_data(self, encrypted_data):
+        try:
+            fernet = Fernet(self.encryption_key)
+            decrypted_data = fernet.decrypt(encrypted_data)
+            return decrypted_data
+        except Exception as e:
+            self.parent_node.debug_message(f"Decryption error: {e}")
+            return None
 def send_data(self, data_payload, encoding_format="utf-8", compression_algorithm='none'):
 	try
