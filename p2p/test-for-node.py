0861b3aef751b41ea90d85552edb1f185de2e8c8
Ashish Konagalla <ashishkonagalla@gmail.com>
2023-12-03 10:17:07 -0500

    Two extra tests updated

--- p2p/test-for-node.py
+++ p2p/test-for-node.py
@@ -4,9 +4,24 @@ import time
 from node import *
 
 if __name__ == "__main__":
-  peer1 = node("0.0.0.0", 4000)
-  peer1.run()
+    
+    # Test to see connection between two nodes
+    node1 = node("0.0.0.0", 4000)
+    node2 = node("0.0.0.0", 4001)
+    
+    node1.start()
+    node2.start()
+    time.sleep(3)
 
-  peer2 = node("0.0.0.0", 4001)
-  peer2.run()
+    # Checking same nodes cannot connect with each other
+    node1.connect("0.0.0.0", 4000)
+    time.sleep(1)
 
+    node1.connect("0.0.0.0", 4001)
+    time.sleep(2)
+
+    node1.stop()
+    node2.stop()
+
+    node1.join()
+    node2.join()
