fd2474ddf22113fceaee781e4196e6c1212fe52a
Ashish Konagalla <ashishkonagalla@gmail.com>
2023-12-03 15:51:40 -0500

    Update test-for-node.py

--- p2p/test-for-node.py
+++ p2p/test-for-node.py
@@ -1,24 +1,34 @@
 import unittest
 import time
 
-from node import *
+from update_node import *
 
 if __name__ == "__main__":
     
     # Test to see connection between two nodes
-    node1 = node("0.0.0.0", 4000)
-    node2 = node("0.0.0.0", 4001)
+    node1 = node("localhost", 4000)
+    node2 = node("localhost", 4001)
     
     node1.start()
     node2.start()
     time.sleep(3)
 
+    total_inbound_nodes_1 = len(node1.inbound_nodes)
+    total_outbound_nodes_1 = len(node1.outbound_nodes)
+    print(total_inbound_nodes_1)
+    print(total_outbound_nodes_1)
+
     # Checking same nodes cannot connect with each other
-    node1.connect("0.0.0.0", 4000)
+    node1.connect("localhost", 4000)
     time.sleep(1)
 
-    node1.connect("0.0.0.0", 4001)
+    node1.connect("localhost", 4001)
     time.sleep(2)
+
+    total_inbound_nodes_2_ = len(node2.inbound_nodes)
+    total_outbound_nodes_2_ = len(node2.outbound_nodes)
+    print(total_inbound_nodes_2_)
+    print(total_outbound_nodes_2_)
 
     node1.stop()
     node2.stop()
